1032fea642e75aa28e7bb0ff6a726b26cd8aab92
mozafari <mozafari@MOZAFARI-PC>
2019-10-10 09:48:18 +0330

    class test

---
+++ myPhoneBook.EndPoints.MVC/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myPhoneBook.EndPoints.MVC
+{
+    public class test
+    {
+        public int name { get; set; }
+    }
+}
